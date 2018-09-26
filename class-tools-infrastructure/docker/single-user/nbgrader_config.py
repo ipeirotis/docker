6ed7505eec31a07e93189246385a5ed14fba4a24
Achilleas Benetopoulos <abenetopoulos@programize.com>
2018-09-26 16:06:29 +0000

    Remove deprecated image

--- class-tools-infrastructure/docker/single-user/nbgrader_config.py
+++
@@ -1,9 +0,0 @@
-import os
-import subprocess
-
-c = get_config()
-
-username = os.getenv('JUPYTERHUB_USER')
-
-c.Exchange.root = '/home/' + username + '/assignments'
-subprocess.call(['sudo', 'chmod', 'ugo+rw', c.Exchange.root])
